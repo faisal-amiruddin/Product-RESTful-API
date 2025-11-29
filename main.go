e909efb9a3ddab1992e95f48649f65594fc624f0
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-11-29 17:40:43 +0700

    installing swag for API documentation

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ import (
     "github.com/joho/godotenv"
 )
 
+// @title           Simple Product REST API
+// @version         1.0
+// @description     REST API menggunakan Fiber dan GORM.
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:3000
+// @BasePath  /api/v1
 func main() {
     if err := godotenv.Load(); err != nil {
         log.Fatal("Error loading .env file")
