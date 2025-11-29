7d80f2e3dde3eb624103eb03f8692f2eec16d9ba
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-11-29 17:18:15 +0700

    add product model

---
+++ models/product.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Book struct {
+    gorm.Model
+    Title string `json:"title"`
+    Description string `json:"description"`
+	Price float64 `json:"price"`
+	Stock int16 `json:"stock"`
+}
